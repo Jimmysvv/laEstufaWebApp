a43f6c45d440a1a278fc7f4772c8545a1bc47575
Bartosz Izdebski <bizdebski>
2018-08-31 16:26:23 +0200

    Initialization, first steps

---
+++ src/main/java/com/laestufa/laEstufa/LaEstufaApplication.java
@@ -0,0 +1,12 @@
+package com.laestufa.laEstufa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaEstufaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaEstufaApplication.class, args);
+	}
+}
